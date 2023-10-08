af1e4b5ad260ddf6fb22efadb2997e1b11499ecc
andrei-mustaca <andrejmustaca6@gmail.com>
2023-10-08 21:15:12 +0300

    Create Program.cs

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+                //Задание
+            //Console.Title = "Тема №4 Компьютерный практикум №1(Мустяца) ";
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.SetWindowSize(60, 25);
+            //Console.WriteLine(Console.CapsLock);
+            //Console.WriteLine(Console.NumberLock);
+            //Console.WriteLine("Введите имя:");
+            //string name=Convert.ToString(Console.ReadLine());
+            //Console.WriteLine($"Здравствуйте {name}");
+            Console.ReadKey();
+    }
+  }
+}
