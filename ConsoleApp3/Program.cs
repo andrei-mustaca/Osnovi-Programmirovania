68954a62fd3034fd4f3afcdd30f22668474c25df
andrei-mustaca <andrejmustaca6@gmail.com>
2023-10-08 22:32:28 +0300

    Create Program.cs

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите 1 слово");
+            //string name1=Convert.ToString(Console.ReadLine());
+            //Console.WriteLine("Введите 2 слово");
+            //string name2 =Convert.ToString(Console.ReadLine());
+            //Console.WriteLine("Введите 3 слово");
+            //string name3 =Convert.ToString(Console.ReadLine());
+            //Console.WriteLine("Введите 4 слово");
+            //string name4 =Convert.ToString(Console.ReadLine());
+            //Console.WriteLine($"{name1}\t{name1}\t");
+            //Console.WriteLine(name3);
+            //Console.WriteLine(""+name4);
+            Console.ReadKey();
+        }
+    }
+}
