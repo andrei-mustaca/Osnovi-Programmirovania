4c0accc8890b2cc5c88b44bb94e1ae7716f0fda5
andrei-mustaca <andrejmustaca6@gmail.com>
2023-10-08 22:19:53 +0300

    Create Program.cs

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            //Console.WriteLine("Ввести координаты точек треугольника:");
+            //Console.Write("x1=");
+            //double x1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("y1=");
+            //double y1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("x2=");
+            //double x2=Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("y2=");
+            //double y2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("x3=");
+            //double x3 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("y3=");
+            //double y3=Convert.ToDouble(Console.ReadLine());
+            //double dlina1 = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+            //double dlina2 = Math.Sqrt(Math.Pow(x3-x2,2)+Math.Pow(y3-y2,2));
+            //double dlina3 = Math.Sqrt(Math.Pow(x1-x3,2)+Math.Pow(y1-y3,2));
+            //double P = 0;
+            //P += dlina1;
+            //P += dlina2;
+            //P += dlina3;
+            //Console.WriteLine("Периметр треугольника="+P);
+            //double p = (dlina1 + dlina2 + dlina3) / 2;
+            //double S = Math.Sqrt(p*(p-dlina1)*(p-dlina2)*(p-dlina3));
+            //Console.WriteLine("Площадь треугольник="+S);
+
+            //Задание 2
+            //Console.WriteLine("Введите расстояние=");
+            //double S = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите расход на 100 км=");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Цена за 1л бензина=");
+            //double c = Convert.ToDouble(Console.ReadLine());
+            //double summa = (S / 100) * b * c;
+            //Console.WriteLine("Сумма поездки="+summa);
+
+            //Задание 3
+            //Console.WriteLine("Цена за 1 кг конфет=");
+            //double sweet = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Цена за 1кг печенья=");
+            //double cookies=Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Цена за 1кг яблок=");
+            //double apple = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Кол-во конфет=");
+            //double x = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Кол-во печенья=");
+            //double y = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Кол-во яблок=");
+            //double z = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"Сумма покупки={sweet*x+cookies*y+apple*z}");
+            Console.ReadKey();
+
+        }
+    }
+}
